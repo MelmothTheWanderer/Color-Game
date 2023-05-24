98a9171e0cb6df90f95032ef88393aa70ed76315
Jamie Simms <jamiesimms@protonmail.com>
2023-05-24 17:01:57 +0100

    added the elements nessecary for the game

--- index.js
+++ index.js
@@ -10,34 +10,19 @@ toggleButton.addEventListener('click', () => {
 
 /* Change the color of the Letters on hover */
 
-/* Step1: Make an array that will house the range of possible colors. I used the colours of 
-the rainbow for now , but they might look better as pastels or more subdued colors */
-
-const colorArray = [ 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-/* Step 2: Using a FOR loop , add an event listener to each of the "spans" in the brand title 
-so that we can change the color when the mouse passes over it */
-
-
+// Define the color spans as as variable
 const listOfSpans=document.getElementsByClassName('color-letter')
-
-// for (let i = 0; i < listOfSpans.length; i ++){
-// let colorArray = ['Brown', 'DarkOrange', 'gold' , 'DarkOliveGreen ', 'LightSeaGreen', 'MediumVioletRed' ,'Violet' ]
-// let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-// listOfSpans[i].style.color=randomColor;
-// }
-
+//Write a function that will Change an elements color property to a random color
 function changeToRandomColor () {
     let colorArray = ['Brown', 'DarkOrange', 'gold' , 'DarkOliveGreen ', 'LightSeaGreen', 'MediumVioletRed' ,'Violet' ];
     let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-    if (this.style.color!= randomColor) {
+    if (this.style.color !== randomColor) {
         this.style.color=randomColor;
     }
     else {
-        this.style.color='black';
+        this.style.color='blue';
     }
 }
-
 
 for (let i = 0 ; i < listOfSpans.length;i++) {
     listOfSpans[i].addEventListener('mouseover', changeToRandomColor);
