97bdb8c310ed70fa915767b89c656dc7442c6ff5
Jamie Simms <jamiesimms@protonmail.com>
2023-05-26 16:58:48 +0100

    added test javascript to test the score , and also started work on javascript that will chose random patterns to display to the user based on the game level

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+/* Scoreboard */
+var scoreboard = document.getElementById("score")
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
@@ -29,17 +31,44 @@ for (let i = 0 ; i < listOfSpans.length;i++) {
 
 }
 
-/*Lets add event listener to all the buttons to test the sounds*/
+/* This is the start of the game. Everything inside of here will be the game , until the game ends */
 
 
 function startGame() {
 
+    /* This gets rid of the start button and displays the game as the game starts */
     document.getElementById('game-container').classList.toggle('hidden');
+    document.getElementsByClassName('start-button-container')[0].classList.toggle('hidden');
+    /* This variable keeps track of the score/level */
+    var gameLevel = 1 
+    /* This function when called, will update the score */
+    function adjustScore () {
+        scoreboard.innerText=gameLevel;
+    }
+    /* todo: write a function that will choose a random pattern based on the game level , then convey this information to the user on screen for them to try and reproduce in 
+    order to score a point and progress to the next level */
+    function revealPattern (level) {
 
+        chosenButtons = []
+        var possibleButtons = [0,1,2,3,4,5,6,7]
+        for (let i = 0;li < level; i ++) { 
+            /*choose a random number from the possibleButtons array and push it to the chosenButtons array */
+            let randomChoice = possibleButtons[Math.floor(Math.random() * possibleButtons.length)];
+            chosenButtons.push(randomChoice)
+         }
+        
+        /*TODO: Now that we have a random arrary , it's a matter of conveying the information on screen to the user */
+
+
+    }
+    adjustScore();
+    /* Adding event listeners to all of the game buttons and playing the corresponding note on click .*/
     for (let i = 0 ; i < document.getElementsByClassName('box').length; i ++ ) { 
         document.getElementsByClassName('box')[i].addEventListener('click', () => {
             var audio = new Audio("./assets/audio/note" + [i] + ".mp3");
             audio.play();
+            gameLevel += 1 ; 
+            adjustScore();
         })
     }
 
