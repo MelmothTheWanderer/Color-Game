7cd8478549a0a694ea7160ab3ac3b0b027130557
Jamie Simms <jamiesimms@protonmail.com>
2023-05-27 13:26:14 +0100

    added javascript to make the game aspect work, but it is still very rough

--- index.js
+++ index.js
@@ -31,8 +31,57 @@ for (let i = 0 ; i < listOfSpans.length;i++) {
 
 }
 
-/* This is the start of the game. Everything inside of here will be the game , until the game ends */
 
+
+/* Making a constant to store the game buttons */ 
+
+var listOfGameButtons = document.getElementsByClassName('box');
+
+/* This function when called, will update the score , pass gameLevel as an argument */
+function adjustScore (score) {
+    scoreboard.innerText=score;
+}
+
+/* This function will return a random array based on the game level */ 
+
+function makeRandomChoices (level) {
+
+    chosenButtons = []
+    var possibleButtons = [0,1,2,3,4,5,6,7]
+    for (let i = 0;i < level; i ++) { 
+        /*choose a random number from the possibleButtons array and push it to the chosenButtons array */
+        let randomChoice = possibleButtons[Math.floor(Math.random() * possibleButtons.length)];
+        chosenButtons.push(randomChoice)
+        
+     }
+     return chosenButtons;
+    }
+
+/*TODO : Write a function that will convey the random array to the user so that they can repeat it . Pass makeRandomChoices as an argument*/ 
+
+function revealPatternToUser (choices) {
+    for (let i = 0 ; i < choices.length ; i++) { 
+
+        
+        function toggleActiveClass () {
+            listOfGameButtons[choices[i]].classList.toggle('active');
+        }
+
+        function playTheNote () {
+         /*Play the music*/
+        var audio = new Audio("./assets/audio/note" + [choices[i]] + ".mp3");
+        audio.play();
+        }
+
+ 
+        /* toggle the class off this time */ 
+        // listOfGameButtons[choices[i]].classList.toggle('active');
+        
+
+    }
+}
+
+/* This is the start of the game. Everything inside of here will be the game , until the game ends */
 
 function startGame() {
 
@@ -41,35 +90,22 @@ function startGame() {
     document.getElementsByClassName('start-button-container')[0].classList.toggle('hidden');
     /* This variable keeps track of the score/level */
     var gameLevel = 1 
-    /* This function when called, will update the score */
-    function adjustScore () {
-        scoreboard.innerText=gameLevel;
-    }
+
     /* todo: write a function that will choose a random pattern based on the game level , then convey this information to the user on screen for them to try and reproduce in 
     order to score a point and progress to the next level */
-    function revealPattern (level) {
 
-        chosenButtons = []
-        var possibleButtons = [0,1,2,3,4,5,6,7]
-        for (let i = 0;li < level; i ++) { 
-            /*choose a random number from the possibleButtons array and push it to the chosenButtons array */
-            let randomChoice = possibleButtons[Math.floor(Math.random() * possibleButtons.length)];
-            chosenButtons.push(randomChoice)
-         }
-        
-        /*TODO: Now that we have a random arrary , it's a matter of conveying the information on screen to the user */
+    revealPatternToUser(makeRandomChoices(gameLevel));
 
 
-    }
-    adjustScore();
+    
     /* Adding event listeners to all of the game buttons and playing the corresponding note on click .*/
-    for (let i = 0 ; i < document.getElementsByClassName('box').length; i ++ ) { 
+    for (let i = 0 ; i < listOfGameButtons.length; i ++ ) { 
         document.getElementsByClassName('box')[i].addEventListener('click', () => {
             var audio = new Audio("./assets/audio/note" + [i] + ".mp3");
             audio.play();
+            /*This is just for testing the score and will be changed later*/
             gameLevel += 1 ; 
-            adjustScore();
+            adjustScore(gameLevel);
         })
     }
-
 }
